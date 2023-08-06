a98e584913e7a5f5c9af5ef3f69bb16d7fa58141
mdmanik74 <manikmhamud74@outlook.com>
2023-08-06 14:50:53 +0600

    flutter setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_daily/screen/ob_bording.dart';
 
-import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: OnBoard(),
     );
   }
 }
