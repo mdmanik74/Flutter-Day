de451ddf5c15adf1319603b8457a0f06c7293a27
mdmanik74 <manikmhamud74@outlook.com>
2023-08-12 13:44:24 +0600

    dialogbo

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
       title: "The Flutter Way",
       theme: ThemeData(
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
             fillColor: Colors.white,
             errorStyle: TextStyle(height: 0),
           )),
+
       home: const OnBoard(),
     );
   }
--- lib/screen/ob_bording.dart
+++ lib/screen/ob_bording.dart
@@ -1,7 +1,4 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
 
 class OnBoard extends StatefulWidget {
   const OnBoard({super.key});
@@ -11,99 +8,31 @@ class OnBoard extends StatefulWidget {
 }
 
 class _OnBoardState extends State<OnBoard> {
-  late RiveAnimationController _btnAnimationController;
-  @override
-  void initState() {
-    _btnAnimationController = OneShotAnimation(
-      "active",
-      autoplay: false,
-    );
-
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Positioned(
-          width: MediaQuery.of(context).size.width * 1.7,
-          bottom: 200,
-          left: 100,
-          child: Image.asset("assets/images/Spline.png"),
-        ),
-        Positioned.fill(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
-            )),
-        RiveAnimation.asset(
-          "assets/images/shapes.riv",
-          controllers: [_btnAnimationController],
-        ),
-        Positioned.fill(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
-              child: const SizedBox(),
-            )),
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 260,
-                  child: Column(children: const [
-                    Text(
-                      "Learn desgin & code",
-                      style: TextStyle(
-                          fontSize: 60, fontFamily: "Poppins", height: 1.2),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                        "Donot skib desgin. Learn desgin and code, by building real apps with Flutter and Swift. Complete courses about the best tools"),
-                  ]),
-                ),
-             SizedBox(
-               height: 64,
-               width: 260,
-               child:    GestureDetector(onTap: (){
+      backgroundColor: Colors.white10,
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Daily Practices',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,color: Colors.black),
 
-                 _btnAnimationController.isActive=true;
-               },),
-             ),
-                SizedBox(
-                  height: 64,
-                  width: 260,
-                  child: Stack(children: [
-                    const RiveAnimation.asset(
-                      "assets/images/button.riv",
-                    ),
-                    Positioned.fill(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(
-                              Icons.arrow_right_outlined,
-                              size: 70,
-                            ),
-                            SizedBox(
-                              width: 8,
-                            ),
-                            Text(
-                              'Start The Course',
-                              style: TextStyle(fontWeight: FontWeight.w600),
-                            )
-                          ],
-                        )),
-                  ]),
-                )
-              ],
-            ),
-          ),
         ),
-      ]),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Show about Dialog'),
+          onPressed: (){ 
+            showDialog(context: context, builder: (context)=> const AboutDialog(
+              applicationIcon: FlutterLogo(),
+              applicationLegalese: 'legalese',
+              children: [
+                Text(('This is a text'))
+              ],
+            ));
+          },
+        ),
+      ),
     );
   }
 }
