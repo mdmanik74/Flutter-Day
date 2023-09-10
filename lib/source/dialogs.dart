08e000e0e5a9703e0cb87efd9a1de36667066325
mdmanik74 <manikmhamud74@outlook.com>
2023-09-10 15:57:42 +0600

    custom dialog

--- lib/source/dialogs.dart
+++ lib/source/dialogs.dart
@@ -5,16 +5,36 @@ class DialogWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text("Success"),
-      titleTextStyle: const TextStyle(
-          fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
-      actionsOverflowButtonSpacing: 20,
-      actions: [
-        ElevatedButton(onPressed: () {}, child: const Text("Back")),
-        ElevatedButton(onPressed: () {}, child: const Text("Next")),
-      ],
-      content: const Text("Saved successfully"),
-    );
-  }
-}
+    return Container(
+  padding: EdgeInsets.only(left: Constants.padding,top: Constants.avatarRadius
+      + Constants.padding, right: Constants.padding,bottom: Constants.padding
+  ),
+  margin: EdgeInsets.only(top: Constants.avatarRadius),
+  decoration: BoxDecoration(
+    shape: BoxShape.rectangle,
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(Constants.padding),
+    boxShadow: [
+      BoxShadow(color: Colors.black,offset: Offset(0,10),
+      blurRadius: 10
+      ),
+    ]
+  ),
+  child: Column(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      Text(widget.title,style: TextStyle(fontSize: 22,fontWeight: FontWeight.w600),),
+      SizedBox(height: 15,),
+      Text(widget.descriptions,style: TextStyle(fontSize: 14),textAlign: TextAlign.center,),
+      SizedBox(height: 22,),
+      Align(
+        alignment: Alignment.bottomRight,
+        child: FlatButton(
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+            child: Text(widget.text,style: TextStyle(fontSize: 18),)),
+      ),
+    ],
+  ),
+),
