263022f5707e8152c3bf304cd4c1a4f97fd8f8c6
mdmanik74 <manikmhamud74@outlook.com>
2023-09-12 16:56:32 +0600

    navigationbar item

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,35 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.amberAccent,
           centerTitle: true,
         ),
-        body: PageViews(),
+        body: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color(0xFF6200EE),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white.withOpacity(.60),
+          selectedFontSize: 14,
+          unselectedFontSize: 14,
+          onTap: (value) {
+            // Respond to item press.
+          },
+          items: [
+            BottomNavigationBarItem(
+              label: 'Favorites',
+              icon: Icon(Icons.favorite),
+            ),
+            BottomNavigationBarItem(
+              label: 'Music',
+              icon: Icon(Icons.music_note),
+            ),
+            BottomNavigationBarItem(
+              label: 'Places',
+              icon: Icon(Icons.location_on),
+            ),
+            BottomNavigationBarItem(
+              label: 'News',
+              icon: Icon(Icons.library_books),
+            ),
+          ],
+        ),
         backgroundColor: Colors.greenAccent,
       ),
     );
