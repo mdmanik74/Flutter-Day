e449d8466f73537f2a182380451bb3b7d617185a
mdmanik74 <manikmhamud74@outlook.com>
2023-09-20 21:15:46 +0600

    e-commerce-menu design

--- lib/ecommerce/home_page.dart
+++ lib/ecommerce/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables, unused_element
 
 import 'package:flutter/material.dart';
 
@@ -12,20 +12,41 @@ class MyEcommersHomePage extends StatefulWidget {
 class _MyEcommersHomePageState extends State<MyEcommersHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: const Column(
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Our Products',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                'Our Products',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              )
+              _buildProductCategory(index: 0, name: 'All Products'),
+              _buildProductCategory(index: 1, name: 'Dress'),
+              _buildProductCategory(index: 2, name: 'Shoes'),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
 }
+
+int isSelected = 0;
+_buildProductCategory({required int index, required String name}) => Container(
+      width: 100,
+      height: 40,
+      margin: const EdgeInsets.only(top: 10, right: 10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isSelected == index ? Colors.red : Colors.black,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        name,
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
