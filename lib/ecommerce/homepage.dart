4db0b0c9002a286526b4de63f42ff39285ebbc45
mdmanik74 <manikmhamud74@outlook.com>
2023-09-24 17:59:35 +0600

    E-commerce appbar search and catgories

--- lib/ecommerce/homepage.dart
+++ lib/ecommerce/homepage.dart
@@ -15,7 +15,56 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(children: [
-        HomeAppBar(),
+        const HomeAppBar(),
+        Container(
+          height: 500,
+          padding: const EdgeInsets.only(top: 15),
+          decoration: const BoxDecoration(
+            color: Color(0xFFEDECF2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+          child: Column(children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Row(children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 5),
+                  height: 50,
+                  width: 300,
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                          border: InputBorder.none, hintText: 'Search here')),
+                ),
+                const Spacer(),
+                const Icon(
+                  Icons.camera_alt,
+                  size: 27,
+                  color: Color(0xFF4C53A5),
+                ),
+              ]),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              child: const Text(
+                'Categories',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5)),
+              ),
+            )
+          ]),
+        ),
       ]),
     );
   }
